6b36488d0b6f605866d81a88028e23c969104990
bohdan-z <58991875+bohdan-z@users.noreply.github.com>
2019-12-19 01:58:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 'use strict';
 
-function calc() {
-    const main = prompt("Выберите математическое действие: 1) плюс, 2) минус, 3) умножить, 4) разделить");
-    const a = prompt("Введите число 1");
-    const b = prompt("Введите число 2");
-    const result = {
-      1 : +a + +b,
-      2 : a - b,
-      3 : a * b,
-      4 : a / b
-    }[main];
-    return result == +result && result | "Непонятно";
-  }
-  alert(calc());
+
 
 
